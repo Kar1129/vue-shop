aea9fc2107c68440f9aa8d3d5dd50e6f9810ae6e
Kar1129 <a1085916700@163.com>
2021-08-10 21:51:36 +0800

    login function completed

---
+++ src/components/Login.vue
@@ -0,0 +1,141 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 登录框头像 -->
+      <div class="avatar_box">
+        <img src="../assets/login-logo.jpg" alt=""/>
+      </div>
+      <!-- 登录表单 -->
+      <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" label-width="0px" class="login_form">
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user" placeholder="用户名3-10位"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input v-model="loginForm.password" prefix-icon="iconfont icon-3702mima" type="password"
+                    placeholder="密码6-15位"></el-input>
+        </el-form-item>
+        <!-- 按钮 -->
+        <el-form-item class="btns">
+          <el-button type="success" plain @click="login">登录</el-button>
+          <el-button type="success" plain v-on:click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      // 登录表单绑定的数据对象
+      loginForm: {
+        username: 'admin',
+        password: '123456'
+      },
+      // 表单验证规则
+      loginFormRules: {
+        username: [{
+          required: true,
+          message: '请输入用户名',
+          // 失去焦点时触发
+          trigger: 'blur'
+        }, {
+          min: 3,
+          max: 10,
+          message: '长度在 3 到 10 个字符',
+          trigger: 'blur'
+        }],
+        password: [{
+          required: true,
+          message: '请输入密码',
+          // 失去焦点时触发
+          trigger: 'blur'
+        }, {
+          min: 6,
+          max: 15,
+          message: '长度在 6 到 15 个字符',
+          trigger: 'blur'
+        }]
+      }
+    }
+  },
+  methods: {
+    // 重置按钮
+    resetLoginForm () {
+      this.$refs.loginFormRef.resetFields()
+    },
+    login () {
+      this.$refs.loginFormRef.validate(async valid => {
+        if (!valid) {
+          return
+        }
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        if (res.meta.status !== 200) {
+          return this.$message.error('登录失败!')
+        }
+        this.$message.success('登录成功!')
+        // 将登录成功之后的 token 保存到客户端的 sessionStorage 中
+        //   项目中除了登录之外的 API 接口，必须在登录成功之后才能访问
+        //   token 只应在当前网站打开期间生效，所以将 token 保存在 sessionStorage 中
+        window.sessionStorage.setItem('token', res.data.token)
+        //  通过编程式导航跳转到后台主页，路由地址是 /home
+        this.$router.push('/home')
+      })
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+.login_container {
+  background-image: url("../assets/login-background.jpg");
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  height: 100%;
+}
+
+// 登录框
+.login_box {
+  width: 450px;
+  height: 300px;
+  border-radius: 3px;
+  background-color: white;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+
+// 登录框头像
+.avatar_box {
+  height: 130px;
+  width: 130px;
+  border: 1px solid white;
+  border-radius: 50% 50%;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 50% 50%;
+  }
+}
+
+// 登录表单
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+
+// 按钮
+.btns {
+  display: flex;
+  justify-content: center;
+}
+</style>
